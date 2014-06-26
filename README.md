e85e808c91f013920ef17d5e99ec8ae2d7a1ae3a
Rob Acheson <rob@fastforwardinnovation.com>
2014-06-26 13:45:32 -0400

    created default js module

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ html-template
 =============
 
 Basic HTML template with Compass, SASS, Grunt, jQuery
+
+run npm install from the project root
--- js/main.js
+++ js/main.js
@@ -1,1 +1,28 @@
-var FF = {} | FF;
+var FF = FF || {};
+
+FF.Main = (function($) {
+
+    var win = $(window),
+        doc = $(document);
+
+    var pub = {},
+        els = {};
+
+    pub.init = init;
+
+    function init() {
+
+        console.log("main.js init");
+
+    }
+
+    return pub;
+
+})(jQuery);
+
+
+$(document).ready(function() {
+
+    FF.Main.init();
+
+});
